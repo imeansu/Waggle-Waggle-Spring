be4eddf78eb87da31730d89c89a24a61914142c2
imeansu <gcnml10@naver.com>
2021-11-09 21:06:03 +0900

    [test] MockMvc test: /member/{memberId}/follower

--- src/test/java/soma/test/waggle/controller/MemberControllerTest.java
+++ src/test/java/soma/test/waggle/controller/MemberControllerTest.java
@@ -1,15 +1,18 @@
 package soma.test.waggle.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import soma.test.waggle.dto.MemberListDto;
 import soma.test.waggle.jwt.TokenProvider;
 import soma.test.waggle.service.MemberService;
@@ -23,21 +26,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest(MemberController.class)
+@WebMvcTest()
+@Import(MemberController.class)
 @ContextConfiguration(classes = {TokenProvider.class})
 @AutoConfigureMockMvc
 public class MemberControllerTest {
 
     @Autowired MockMvc mvc;
 
-    @Autowired TokenProvider tokenProvider;
-
-    private String jwt = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMSIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE2MzYzMDQ4Njl9.C7_W4uaCkEARowkx2vVKVl0caFHwLAE1Q-VwkMWB48_hJSEcKIuKOXUOH0RNDEJ1eJ19F7fBokb48a5cBuuEqA";
+    private String jwt = " Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMSIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE2MzYzMDQ4Njl9.C7_W4uaCkEARowkx2vVKVl0caFHwLAE1Q-VwkMWB48_hJSEcKIuKOXUOH0RNDEJ1eJ19F7fBokb48a5cBuuEqA";
 
     @MockBean private MemberService memberService;
 
     @WithMockUser(username = "1")
-//    @Test
+    @Test
     public void 팔로우_멤버_조회() throws Exception {
         // given
         MemberListDto memberListDto = MemberListDto.builder()
@@ -49,7 +51,7 @@ public class MemberControllerTest {
                 .willReturn(memberListDto);
 
         // when
-        final ResultActions actions = mvc.perform(get("/basics/language-list")
+        final ResultActions actions = mvc.perform(get("/member/1/follower")
                 .header("Authorization", jwt)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print());
