c844cbe7b163224b154792bc8976de5ab5405593
imeansu <gcnml10@naver.com>
2021-11-15 13:35:24 +0900

    [fix] timezone: Asia/Seoul

--- src/main/java/soma/test/waggle/WaggleApplication.java
+++ src/main/java/soma/test/waggle/WaggleApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 //@SpringBootApplication
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class WaggleApplication {
@@ -12,4 +15,9 @@ public class WaggleApplication {
 		SpringApplication.run(WaggleApplication.class, args);
 	}
 
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
+	}
 }
