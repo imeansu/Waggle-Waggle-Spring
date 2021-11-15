2ad86e2b5249c5a2596dac6ee25f8248a9d9c84c
imeansu <gcnml10@naver.com>
2021-11-15 14:36:13 +0900

    [fix] timezone and logging

--- src/main/java/soma/test/waggle/WaggleApplication.java
+++ src/main/java/soma/test/waggle/WaggleApplication.java
@@ -1,14 +1,17 @@
 package soma.test.waggle;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import javax.annotation.PostConstruct;
+import java.util.Date;
 import java.util.TimeZone;
 
-//@SpringBootApplication
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
+@Slf4j
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class WaggleApplication {
 
 	public static void main(String[] args) {
@@ -18,6 +21,6 @@ public class WaggleApplication {
 	@PostConstruct
 	public void started(){
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-
+		log.info("현재 시각 : {}", new Date());
 	}
 }
